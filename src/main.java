2277b7030e18675cbb952f0ed945bc792e810dab
havedupont <gustavspauls4life@gmail.com>
2025-11-28 17:05:46 +0200

    input

--- src/main.java
+++ src/main.java
@@ -7,40 +7,49 @@ import java.io.*;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 
-
 public class main {
-	public static void main(String[] args) {
-                // Define variables
-                String Nosaukums,Valsts;
-                int Budget,Ilgums;
-                String[] Aktiviitate;
-                // Create JSON structure
-                JSONObject root = new JSONObject();
-                JSONArray celojumi = new JSONArray();
-                JSONObject dest = new JSONObject();
-                // Sample data       
-                dest.put("Nosaukums", "TEST-Nosaukums");
-                dest.put("Valsts", "TEST-Valsts");
-                dest.put("Budget", 500);
-                dest.put("Ilgums-Dienas", 6);
-                
-                // aktivitates array
-                JSONArray aktivitates = new JSONArray();
-                // [] Add loop for array activities
-                aktivitates.add("muzejs");
-                aktivitates.add("pludmale");
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // Define variables
+        String Nosaukums, Valsts;
+        int Budget, Ilgums;
+        String[] Aktiviitate;
 
+        System.out.println("Ievadi pilsetu ");
+        Nosaukums = scan.next();
+        System.out.println("Ievadi valsti ");
+        Valsts = scan.next();
+        System.out.println("Ievadi ilgumu (dienas) ");
+        Ilgums = scan.nextInt();
+        System.out.println("Ievadi budzetu ");
+        Budget = scan.nextInt();
+        // System.out.println("Ievadi aktivitates ");
+        // Create JSON structure
+        JSONObject root = new JSONObject();
+        JSONArray celojumi = new JSONArray();
+        JSONObject dest = new JSONObject();
+        // Sample data
+        dest.put("Nosaukums", "TEST-Nosaukums");
+        dest.put("Valsts", "TEST-Valsts");
+        dest.put("Budget", 500);
+        dest.put("Ilgums-Dienas", 6);
 
-                dest.put("Aktivitate", aktivitates);
-                
-                celojumi.add(dest);
+        // aktivitates array
+        JSONArray aktivitates = new JSONArray();
+        // [] Add loop for array activities
+        aktivitates.add("muzejs");
+        aktivitates.add("pludmale");
 
-                root.put("Celojumi", celojumi);
-                try (FileWriter file = new FileWriter("celojumi.json")) {
-                    file.write(root.toJSONString());
-                    file.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-	}
+        dest.put("Aktivitate", aktivitates);
+
+        celojumi.add(dest);
+
+        root.put("Celojumi", celojumi);
+        try (FileWriter file = new FileWriter("celojumi.json")) {
+            file.write(root.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
